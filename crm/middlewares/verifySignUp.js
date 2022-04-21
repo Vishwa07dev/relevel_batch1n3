aac69b269b380369934a4d7a6c7c82a1802e6f4c
suvotheking007 <suvotheking007@gmail.com>
2022-04-21 21:58:55 +0530

    Added email validator when the user is trying to signup

--- crm/middlewares/verifySignUp.js
+++ crm/middlewares/verifySignUp.js
@@ -1,90 +1,103 @@
 /**
- * This file will contain the custom middleware for 
+ * This file will contain the custom middleware for
  * varifying the request body
  */
 
-const User = require("../models/user.model")
+const User = require("../models/user.model");
 const constant = require("../utils/constants");
+const emailChecker = require("../utils/emailChecker");
 
-validateSignupRequest = async (req,res, next) =>{
-    //Validate if userName exists
-    if(!req.body.name){
-        return res.status(400).send({
-            message : "Failed !  User name is not provided"
-        })
-    }
+validateSignupRequest = async (req, res, next) => {
+  //Validate if userName exists
+  if (!req.body.name) {
+    return res.status(400).send({
+      message: "Failed !  User name is not provided",
+    });
+  }
 
-    //Validate if the userId exists
-    if(!req.body.userId){
-        return res.status(400).send({
-            message : "Failed !  User Id is not provided"
-        })
-    }
+  //Validate if the userId exists
+  if (!req.body.userId) {
+    return res.status(400).send({
+      message: "Failed !  User Id is not provided",
+    });
+  }
 
-    /**
-     * Valiate if the userIs is already not preset
-     */
-    const user = await User.findOne({userId : req.body.userId});
+  /**
+   * Valiate if the userIs is already not preset
+   */
+  const user = await User.findOne({ userId: req.body.userId });
 
-    if(user!=null){
-        return res.status(400).send({
-            message : "Failed !  User Id already exist"
-        })
-    }
+  if (user != null) {
+    return res.status(400).send({
+      message: "Failed !  User Id already exist",
+    });
+  }
 
-    if(!req.body.email){
-        return res.status(400).send({
-            message : "Failed !  User Email Id is not provided"
-        })
-    }
+  if (!req.body.email) {
+    return res.status(400).send({
+      message: "Failed !  User Email Id is not provided",
+    });
+  }
 
-    /**
-     * if the email id is already existing
-     */
+  /**
+   * if the email id is already existing
+   */
 
-    const email = await User.findOne({email : req.body.email});
-    if(email!=null){
-        return res.status(400).send({
-            message : "Failed !  Email ID already exists"
-        }) 
-    }
+  const email = await User.findOne({ email: req.body.email });
+  if (email != null) {
+    return res.status(400).send({
+      message: "Failed !  Email ID already exists",
+    });
+  }
 
-    if(!req.body.password){
-        return res.status(400).send({
-            message : "Failed !  User password is not provided"
-        })
-    }
+  if (!req.body.password) {
+    return res.status(400).send({
+      message: "Failed !  User password is not provided",
+    });
+  }
 
-    /**
+  /**
      * Validation for the use type
      * customer : "CUSTOMER",
         admin : "ADMIN",
         engineer
      */
-    const userType = req.body.userType ;
-    const userTypes = [ constant.userTypes.customer , constant.userTypes.admin, constant.userTypes.engineer]
-    if(userType && !userTypes.includes(userType)){
-        return res.status(400).send({
-            message : "Failed !  User type is not correctly provided"
-        })
-    }
-    
-    /**
-     * similar validation for all the other fields
-     * 
-     * email, 
-     * password,
-     * userType
-     */
-    next(); // give the controll to the controller
+  const userType = req.body.userType;
+  const userTypes = [
+    constant.userTypes.customer,
+    constant.userTypes.admin,
+    constant.userTypes.engineer,
+  ];
+  if (userType && !userTypes.includes(userType)) {
+    return res.status(400).send({
+      message: "Failed !  User type is not correctly provided",
+    });
+  }
 
-    /**
-     * Scope of improving the code :
-     * 
-     *     Validate if the email id is in correct format :  abc@xyz.com   , adcw1313
-     */
-}
+  /**
+   * similar validation for all the other fields
+   *
+   * email,
+   * password,
+   * userType
+   */
+
+  /**
+   * Scope of improving the code :
+   *
+   *     Validate if the email id is in correct format :  abc@xyz.com   , adcw1313
+   */
+  const userEmail = req.body.email;
+  const emailValidity = emailChecker(userEmail);
+  if (!emailValidity.valid) {
+    return res.status(400).send({
+      message: "Entered email is invalid",
+    });
+  }
+
+  next(); // give the controll to the controller
+};
 
 module.exports = {
-    validateSignUpRequest : validateSignupRequest
-}
+  validateSignUpRequest: validateSignupRequest,
+};
