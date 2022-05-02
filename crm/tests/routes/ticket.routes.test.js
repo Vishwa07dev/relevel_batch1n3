69d6f703a87e4cf4bda41e040cc9e59e31d3702e
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2022-05-02 21:51:10 +0530

    test codes

--- crm/tests/routes/ticket.routes.test.js
+++ crm/tests/routes/ticket.routes.test.js
@@ -13,7 +13,8 @@ const client = require("../../utils/NotificationServiceClient").client;
 const request = require("supertest");
 const app = require("../../server");
 const { ticketPriority } = require("../../utils/constants");
-const jwt =require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
+const Ticket = require("../../models/ticket.model");
 /**
  * I need to have the server
  */
@@ -23,7 +24,7 @@ beforeAll(async () => {
     /**
      * connection
      */
-    
+
     /**
      * This will be executed before all the tests
      */
@@ -62,6 +63,15 @@ const ticketRequestBody = {
     assignee: 1
 }
 
+const ticketUpdateRequestBody = {
+    title: "Test",
+    ticketPriority: 4,
+    description: "Test",
+    status: "CLOSED",
+    assignee: 1
+}
+var ticketId;
+
 describe("testing the POST Ticket creation endpoint", () => {
 
     const apiEndpoint = "/crm/api/v1/tickets";
@@ -82,7 +92,7 @@ describe("testing the POST Ticket creation endpoint", () => {
     jest.spyOn(client, 'post').mockImplementation((url, args, cb) => cb("Test", null));
 
 
-    it("I should be able to successfully create a ticket",async () => {
+    it("I should be able to successfully create a ticket", async () => {
 
         /**
          * Call the API
@@ -93,9 +103,42 @@ describe("testing the POST Ticket creation endpoint", () => {
         const res = await request(app).post(apiEndpoint)
             .set("x-access-token", token).send(ticketRequestBody);
 
+
+        ticketId = res.body.id;
+        console.log(ticketId);
         expect(res.statusCode).toEqual(201);
 
 
     });
 
 });
+
+
+
+describe("Update ticket : PUT API", () => {
+
+    /**
+     * Mock the notification service
+     */
+    jest.spyOn(client, 'post').mockImplementation((url, args, cb) => cb("Test", null));
+
+    const token = jwt.sign({ id: 1 }, config.secret, {
+        expiresIn: 600
+    });
+
+
+
+    it("I should be able to update", async () => {
+
+        const createdTicket = await Ticket.create(ticketRequestBody);
+        console.log(createdTicket._id);
+        const apiEndpoint = "/crm/api/v1/tickets/" + createdTicket._id;
+        const res = await request(app).put(apiEndpoint)
+            .set("x-access-token", token).send(ticketUpdateRequestBody);
+
+        expect(res.statusCode).toEqual(200);
+
+    });
+
+
+})
