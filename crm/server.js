7b6ac0a0e020f845888109d167ec738d3da91a4c
suvotheking007 <suvotheking007@gmail.com>
2022-05-05 14:34:43 +0530

    Cleanup due to seperation of the logic to represent the default admin user into another file

--- crm/server.js
+++ crm/server.js
@@ -3,9 +3,7 @@ const serverConfig = require("./configs/server.config");
 const mongoose = require("mongoose");
 const dbConfig = require("./configs/db.config");
 const bodyParser = require("body-parser");
-const bcrypt = require("bcryptjs");
-const User = require("./models/user.model");
-
+const rootAdmin = require("./utils/rootAdmin");
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,39 +12,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
  * Setup the mongodb connection and create on ADMIN user
  */
 mongoose.connect(dbConfig.DB_URL, () => {
-    console.log("MongoDB connected");
-    //Initialization
-    init();
-})
+  console.log("MongoDB connected");
 
-async function init() {
-
-    var user = await User.findOne({ userId: "admin" });
-
-    if (user) {
-        return;
-    } else {
-
-        //Create the admin user
-
-        const user = await User.create({
-            name: "Vish",
-            userId: "admin",
-            email: "kankvish7777@gmail.com",
-            userType: "ADMIN",
-            password: bcrypt.hashSync("Welcome00123", 8)
-        });
-        console.log("admin user is created");
-
+  //Initialization of the default admin user
+  rootAdmin.init((admin, error) => {
+    if (error) {
+      console.log(
+        "The following error occurred while the root admin was getting initialized : \n",
+        error
+      );
     }
-}
+    console.log(
+      "Root admin initialized with the following details : \n ",
+      admin
+    );
+  });
+});
 
-
-
-
-require('./routes/auth.routes')(app);
-require('./routes/user.routes')(app);
-require('./routes/ticket.routes')(app);
+require("./routes/auth.routes")(app);
+require("./routes/user.routes")(app);
+require("./routes/ticket.routes")(app);
 /**
  * Start the express server,
  * Need to export it so that it can be
@@ -54,5 +39,5 @@ require('./routes/ticket.routes')(app);
  */
 
 module.exports = app.listen(serverConfig.PORT, () => {
-    console.log("Application has started on the port ", serverConfig.PORT);
-})
+  console.log("Application has started on the port ", serverConfig.PORT);
+});
