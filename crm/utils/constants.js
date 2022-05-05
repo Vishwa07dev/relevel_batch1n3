ee47852644e2f8dea5edc8c424a6ebd7ded68ac1
suvotheking007 <suvotheking007@gmail.com>
2022-05-05 13:24:03 +0530

    Rectified a small commitment mistake

--- crm/utils/constants.js
+++ crm/utils/constants.js
@@ -43,29 +43,3 @@ exports.ticketStatusEnum = Object.entries(this.ticketStatus).map(
 exports.ticketPriorityEnum = Object.entries(this.ticketPriority).map(
   (result) => result[1]
 );
-
-module.exports = {
-  userTypes: {
-    customer: "CUSTOMER",
-    admin: "ADMIN",
-    engineer: "ENGINEER",
-  },
-  userStatus: {
-    pending: "PENDING",
-    approved: "APPROVED",
-    rejected: "REJECTED",
-  },
-  ticketStatus: {
-    open: "OPEN",
-    closed: "CLOSED",
-    blocked: "BLOCKED",
-    inProgress: "IN_PROGRESS",
-  },
-  ticketPriority: {
-    one: 1,
-    two: 2,
-    three: 3,
-    four: 4,
-  },
-  ticketStatusEnum: Object.entries(this.userStatus),
-};
