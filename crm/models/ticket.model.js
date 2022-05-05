ff9a88859a71b3e49d77375799f8464a6e1c83d7
suvotheking007 <suvotheking007@gmail.com>
2022-05-05 10:25:16 +0530

    Made reporter field mandatory since sending email notification to user depends on this

--- crm/models/ticket.model.js
+++ crm/models/ticket.model.js
@@ -2,49 +2,49 @@
  * This file represents the scehma for the ticket resource
  */
 
-
 const mongoose = require("mongoose");
 const constants = require("../utils/constants");
 
 const ticketSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  ticketPriority: {
+    type: Number,
+    required: true,
+    default: constants.ticketPriority.four, // Possible values : 1/2/3/4
+  },
+  status: {
+    type: String,
+    required: true,
+    default: constants.ticketStatus.open, // Possible values : "OPEN/CLOSED/BLOCKED"
+  },
+  reporter: {
+    // Who created ticket - userId of the user who created
+    type: String,
+    required: true,
+  },
+  assignee: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    immutable: true,
+    default: () => {
+      return Date.now();
     },
-    description: {
-        type: String,
-        required: true
+  },
+  updatedAt: {
+    type: Date,
+    default: () => {
+      return Date.now();
     },
-    ticketPriority: {
-        type: Number,
-        required: true,
-        default: constants.ticketPriority.four // Possible values : 1/2/3/4
-    },
-    status: {
-        type: String,
-        required: true,
-        default: constants.ticketStatus.open // Possible values : "OPEN/CLOSED/BLOCKED"
-    },
-    reporter: {  // Who created ticket - userId of the user who created
-        type: String
-    },
-    assignee: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        immutable: true,
-        default: () => {
-            return Date.now();
-        }
-
-    },
-    updatedAt: {
-        type: Date,
-        default: () => {
-            return Date.now();
-        }
-    }
+  },
 });
 
 module.exports = mongoose.model("Ticket", ticketSchema);
